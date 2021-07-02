b1a9cce786e3db72bd9d22cf03d7fe19abcee0b1
Andreas Arvidsson <andreas.arvidsson@redpill-linpro.com>
2021-07-02 21:46:39 +0200

    removed scroll feature in favor of internal command

--- src/extension.js
+++ src/extension.js
@@ -2,15 +2,12 @@ const vscode = require("vscode");
 const selectTo = require("./select-to");
 const lineMiddle = require("./line-middle");
 const formatDocument = require("./format-document");
-const scroll = require("./scroll.js");
 
 const activate = (context) => {
     context.subscriptions.push(
         vscode.commands.registerCommand("andreas.selectTo", selectTo),
         vscode.commands.registerCommand("andreas.lineMiddle", lineMiddle),
-        vscode.commands.registerCommand("andreas.formatDocument", formatDocument),
-        vscode.commands.registerCommand("andreas.scrollUpHalfPage", scroll.upHalfPage),
-        vscode.commands.registerCommand("andreas.scrollDownHalfPage", scroll.downHalfPage)
+        vscode.commands.registerCommand("andreas.formatDocument", formatDocument)
     );
 };
 
