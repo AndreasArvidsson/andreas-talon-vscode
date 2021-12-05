89568f088eb07450368f3d678efc6d94fb5d4565
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-12-05 08:09:43 +0100

    Updated line middle to support multiple cursors

--- src/lineMiddle.ts
+++ src/lineMiddle.ts
@@ -2,17 +2,17 @@ import { window, Selection } from "vscode";
 
 export default () => {
     const editor = window.activeTextEditor!;
-    const pos = editor.selection.active;
-    const text = editor.document.lineAt(pos.line).text;
-    const trimText = text.trim();
-    let start, end;
-    if (trimText) {
-        start = text.indexOf(trimText[0]);
-        end = start + trimText.length;
-    } else {
-        start = 0;
-        end = text.length;
-    }
-    const middle = Math.floor((start + end) / 2);
-    editor.selection = new Selection(pos.line, middle, pos.line, middle);
+    editor.selections = editor.selections.map(selection => {
+        const line = editor.document.lineAt(selection.active.line);
+        let start, end;
+        if (line.isEmptyOrWhitespace) {
+            start = 0;
+            end = line.text.length;
+        } else {
+            start = line.firstNonWhitespaceCharacterIndex;
+            end = start + line.text.trim().length;
+        }
+        const middle = Math.floor((start + end) / 2);
+        return new Selection(line.lineNumber, middle, line.lineNumber, middle);
+    });
 };
