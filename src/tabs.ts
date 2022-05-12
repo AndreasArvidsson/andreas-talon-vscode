ba038e192d06f10baedcc7be35382a44aee9d50f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-05-12 04:30:21 +0200

    Cleanup

--- src/tabs.ts
+++ src/tabs.ts
@@ -1,11 +1,9 @@
 import { window, commands } from "vscode";
-import * as vscode from "vscode";
 
 export const openEditorAtIndex = async (index: number) => {
     // Negative index starts from the back
     if (index < 0) {
-        const tabGroups = (window as any).tabGroups;
-        const numTabs: number = tabGroups.activeTabGroup.tabs.length;
+        const numTabs: number = window.tabGroups.activeTabGroup.tabs.length;
         index += numTabs;
     }
     await commands.executeCommand("workbench.action.openEditorAtIndex", index);
