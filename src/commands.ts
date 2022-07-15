a0c892c23c3c642480446ce6e69ac6c0ad74f7b2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-07-15 16:58:23 +0200

    Changed the argument to execute commands to string list

--- src/commands.ts
+++ src/commands.ts
@@ -1,17 +1,13 @@
-import { commands } from "vscode";
+import * as vscode from "vscode";
 
-interface Args {
-    commands: string[];
-}
-
-export async function executeCommands(args: Args) {
-    for (const command of args.commands) {
-        await commands.executeCommand(command);
+export async function executeCommands(commands: string[]) {
+    for (const command of commands) {
+        await vscode.commands.executeCommand(command);
     }
 }
 
 export async function printCommands() {
-    let commandList = await commands.getCommands();
+    let commandList = await vscode.commands.getCommands();
     commandList = commandList.filter((c) => !c.startsWith("_"));
     commandList.sort();
     commandList.forEach((c) => {
