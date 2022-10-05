bb3b42d1cc9109cffd179cfeb7b94110b4b3f636
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2022-10-05 12:15:46 +0200

    Updated generate range to leave selection after generated numbers

--- src/generateRange.ts
+++ src/generateRange.ts
@@ -1,4 +1,4 @@
-import { window } from "vscode";
+import { Selection, window } from "vscode";
 import getSortedSelections from "./util/getSortedSelections";
 
 export default async (start: number = 0) => {
@@ -8,9 +8,18 @@ export default async (start: number = 0) => {
         return;
     }
 
+    const offsets: number[] = [];
+
+    // This is the delta in offset due to changes earlier in the document.
+    let offsetDelta = 0;
+
     const wereEditsApplied = await editor.edit((editBuilder) => {
         getSortedSelections().forEach((selection, i) => {
+            const offsetStart = editor.document.offsetAt(selection.start);
+            const offsetEnd = editor.document.offsetAt(selection.end);
             const text = (start + i).toString();
+            offsets.push(offsetStart + text.length + offsetDelta);
+            offsetDelta += text.length - (offsetEnd - offsetStart);
             editBuilder.replace(selection, text);
         });
     });
@@ -18,4 +27,9 @@ export default async (start: number = 0) => {
     if (!wereEditsApplied) {
         throw Error("Couldn't apply edits for generate range");
     }
+
+    editor.selections = offsets.map((offset) => {
+        const position = editor.document.positionAt(offset);
+        return new Selection(position, position);
+    });
 };
