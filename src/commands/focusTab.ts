f3a6192db23b180e5429c73f781d6f9ea0355552
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-08-05 07:39:23 +0200

    cleanup focus tab command

--- src/commands/focusTab.ts
+++ src/commands/focusTab.ts
@@ -1,28 +1,31 @@
-import { commands, window } from "vscode";
+import { TabGroup, commands, window } from "vscode";
 import { focusViewColumn } from "../util/focusViewColumn";
 import { hintToIndex } from "../util/hints";
 
 export async function focusTab(hint: string): Promise<void> {
     const index = hintToIndex(hint);
-    const indexInGroup = await focusTabGroup(index);
+    const tabInfo = getTabInfo(index);
 
-    await commands.executeCommand("workbench.action.openEditorAtIndex", indexInGroup);
+    if (tabInfo == null) {
+        throw Error(`Can't focus non-existing tab '${hint.toUpperCase()}' at index '${index}'`);
+    }
+
+    if (!tabInfo.group.isActive) {
+        await focusViewColumn(tabInfo.group.viewColumn);
+    }
+
+    await commands.executeCommand("workbench.action.openEditorAtIndex", tabInfo.index);
 }
 
-async function focusTabGroup(index: number): Promise<number> {
-    const tabGroups = window.tabGroups.all;
-
+function getTabInfo(index: number): { group: TabGroup; index: number } | null {
     let groupIndex = 0;
 
-    for (const group of tabGroups) {
+    for (const group of window.tabGroups.all) {
         if (index < groupIndex + group.tabs.length) {
-            if (!group.isActive) {
-                await focusViewColumn(group.viewColumn);
-            }
-            return index - groupIndex;
+            return { group, index: index - groupIndex };
         }
         groupIndex += group.tabs.length;
     }
 
-    throw Error(`Can't focus non-existing tab at index ${index}`);
+    return null;
 }
