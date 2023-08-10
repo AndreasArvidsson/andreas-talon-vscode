81dfd36cb00238c7da996e6066c70250b741b3d5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-08-10 13:53:48 +0200

    Refactored formatters with interface

---
+++ src/language/TalonFormatter.ts
@@ -0,0 +1,136 @@
+import type { SyntaxNode } from "web-tree-sitter";
+import type { LanguageFormatter } from "./registerLanguageFormatter";
+
+const columnWidth = 28;
+
+export class TalonFormatter implements LanguageFormatter {
+    private lastRow: number;
+    private ident = "";
+    private eol = "";
+
+    constructor() {
+        this.lastRow = 0;
+        this.getNodeText = this.getNodeText.bind(this);
+    }
+
+    getText(ident: string, eol: string, node: SyntaxNode): string {
+        this.ident = ident;
+        this.eol = eol;
+        return this.getNodeText(node) + this.eol;
+    }
+
+    private getLeftRightText(node: SyntaxNode): string {
+        const { children } = node;
+        const isMultiline = children[2].startPosition.row > children[1].endPosition.row;
+        const left = this.getNodeText(children[0]);
+        const leftWithColon = `${left}:`;
+        const leftWithPadding = isMultiline
+            ? leftWithColon
+            : `${leftWithColon} `.padEnd(columnWidth);
+        const nl = isMultiline ? this.eol : "";
+        const right = children
+            .slice(2)
+            .map((n) => this.getNodeText(n, isMultiline))
+            .join(this.eol);
+        return `${leftWithPadding}${nl}${right}`;
+    }
+
+    private getNodeText(node: SyntaxNode, isIndented = false): string {
+        const nl = node.startPosition.row > this.lastRow + 1 ? this.eol : "";
+        this.lastRow = node.endPosition.row;
+        const text = this.getNodeTextInternal(node, isIndented);
+        this.lastRow = node.endPosition.row;
+        return `${nl}${text}`;
+    }
+
+    private getNodeTextInternal(node: SyntaxNode, isIndented = false): string {
+        switch (node.type) {
+            case "source_file":
+                return node.children
+                    .map((n) => this.getNodeText(n))
+                    .filter(Boolean)
+                    .join(this.eol);
+
+            case "matches": {
+                if (node.children.length === 0) {
+                    return "";
+                }
+                const text = node.children.map((n) => this.getNodeText(n)).join(this.eol);
+                return `${text}${this.eol}-`;
+            }
+
+            case "declarations":
+                return node.children.map((n) => this.getNodeText(n)).join(this.eol);
+
+            case "match":
+                return node.children.map((n) => this.getNodeText(n)).join("");
+
+            case "block":
+                return node.children.map((n) => this.getNodeText(n, isIndented)).join(this.eol);
+
+            case "command_declaration":
+            case "key_binding_declaration":
+            case "parrot_declaration":
+            case "noise_declaration":
+            case "face_declaration":
+            case "gamepad_declaration":
+            case "settings_declaration":
+                return this.getLeftRightText(node);
+
+            case "comment": {
+                // When using crlf eol comments have a trailing `\r`
+                const text = node.text.trimEnd();
+                return isIndented ? `${this.ident}${text}` : text;
+            }
+
+            case "expression_statement":
+            case "assignment_statement": {
+                const text = node.children.map((n) => this.getNodeText(n)).join(" ");
+                return isIndented ? `${this.ident}${text}` : text;
+            }
+
+            case "action":
+            case "key_action":
+            case "sleep_action":
+            case "argument_list":
+            case "key_binding":
+            case "face_binding":
+            case "gamepad_binding":
+            case "parrot_binding":
+            case "noise_binding":
+            case "tag_import_declaration":
+                return node.children.map((n) => this.getNodeText(n)).join("");
+
+            case "match_modifier":
+            case ":":
+            case ",":
+                return `${node.text} `;
+
+            case "implicit_string":
+                return node.text.trim();
+
+            case "rule":
+            case "tag_binding":
+            case "settings_binding":
+            case "key(":
+            case "sleep(":
+            case "gamepad(":
+            case "face(":
+            case "parrot(":
+            case "noise(":
+            case "identifier":
+            case "variable":
+            case "string":
+            case "integer":
+            case "float":
+            case "=":
+            case "(":
+            case ")":
+                return node.text;
+
+            default:
+                console.warn(`Unknown syntax node type '${node.type}'`);
+                return node.text;
+        }
+    }
+}
--- src/language/registerLanguageFormatter.ts
+++ src/language/registerLanguageFormatter.ts
@@ -1,197 +1,59 @@
-/* eslint-disable @typescript-eslint/unbound-method */
-import { Disposable, EndOfLine, languages, Range, TextEdit, TextEditor, window } from "vscode";
-import { SyntaxNode } from "web-tree-sitter";
+import {
+    Disposable,
+    EndOfLine,
+    FormattingOptions,
+    languages,
+    Range,
+    TextDocument,
+    TextEdit
+} from "vscode";
+import type { SyntaxNode } from "web-tree-sitter";
 import { ParseTreeExtension } from "../typings/parserTree";
+import { TalonFormatter } from "./TalonFormatter";
 
-const columnWidth = 28;
-
-class TalonFormatter {
-    private readonly ident: string;
-    private readonly eol: string;
-    private lastRow: number;
-
-    constructor(private editor: TextEditor) {
-        this.ident = getIndentation(editor);
-        this.eol = getEOL(editor);
-        this.lastRow = 0;
-        this.getNodeText = this.getNodeText.bind(this);
-    }
-
-    getText(node: SyntaxNode): string {
-        return this.getNodeText(node) + this.eol;
-    }
-
-    private getLeftRightText(node: SyntaxNode): string {
-        const { children } = node;
-        const isMultiline = children[2].startPosition.row > children[1].endPosition.row;
-        const left = this.getNodeText(children[0]);
-        const leftWithColon = `${left}:`;
-        const leftWithPadding = isMultiline
-            ? leftWithColon
-            : `${leftWithColon} `.padEnd(columnWidth);
-        const nl = isMultiline ? this.eol : "";
-        const right = children
-            .slice(2)
-            .map((n) => this.getNodeText(n, isMultiline))
-            .join(this.eol);
-        return `${leftWithPadding}${nl}${right}`;
-    }
-
-    private getNodeText(node: SyntaxNode, isIndented = false): string {
-        const nl = node.startPosition.row > this.lastRow + 1 ? this.eol : "";
-        this.lastRow = node.endPosition.row;
-        const text = this.getNodeTextInternal(node, isIndented);
-        this.lastRow = node.endPosition.row;
-        return `${nl}${text}`;
-    }
-
-    private getNodeTextInternal(node: SyntaxNode, isIndented = false): string {
-        switch (node.type) {
-            case "source_file":
-                return node.children
-                    .map((n) => this.getNodeText(n))
-                    .filter(Boolean)
-                    .join(this.eol);
-
-            case "matches": {
-                if (node.children.length === 0) {
-                    return "";
-                }
-                const text = node.children.map((n) => this.getNodeText(n)).join(this.eol);
-                return `${text}${this.eol}-`;
-            }
-
-            case "declarations":
-                return node.children.map((n) => this.getNodeText(n)).join(this.eol);
-
-            case "match":
-                return node.children.map((n) => this.getNodeText(n)).join("");
-
-            case "block":
-                return node.children.map((n) => this.getNodeText(n, isIndented)).join(this.eol);
-
-            case "command_declaration":
-            case "key_binding_declaration":
-            case "parrot_declaration":
-            case "noise_declaration":
-            case "face_declaration":
-            case "gamepad_declaration":
-            case "settings_declaration":
-                return this.getLeftRightText(node);
-
-            case "comment": {
-                // When using crlf eol comments have a trailing `\r`
-                const text = node.text.trimEnd();
-                return isIndented ? `${this.ident}${text}` : text;
-            }
-
-            case "expression_statement":
-            case "assignment_statement": {
-                const text = node.children.map((n) => this.getNodeText(n)).join(" ");
-                return isIndented ? `${this.ident}${text}` : text;
-            }
-
-            case "action":
-            case "key_action":
-            case "sleep_action":
-            case "argument_list":
-            case "key_binding":
-            case "face_binding":
-            case "gamepad_binding":
-            case "parrot_binding":
-            case "noise_binding":
-            case "tag_import_declaration":
-                return node.children.map((n) => this.getNodeText(n)).join("");
-
-            case "match_modifier":
-            case ":":
-            case ",":
-                return `${node.text} `;
-
-            case "implicit_string":
-                return node.text.trim();
-
-            case "rule":
-            case "tag_binding":
-            case "settings_binding":
-            case "key(":
-            case "sleep(":
-            case "gamepad(":
-            case "face(":
-            case "parrot(":
-            case "noise(":
-            case "identifier":
-            case "variable":
-            case "string":
-            case "integer":
-            case "float":
-            case "=":
-            case "(":
-            case ")":
-                return node.text;
-
-            default:
-                console.warn(`Unknown syntax node type '${node.type}'`);
-                return node.text;
-        }
-    }
+export interface LanguageFormatter {
+    getText(ident: string, eol: string, node: SyntaxNode): string;
 }
 
-function provideDocumentFormattingEditsTalon(parseTreeExtension: ParseTreeExtension): TextEdit[] {
-    const editor = window.activeTextEditor;
-
-    if (editor == null) {
-        return [];
-    }
-
-    const tree = parseTreeExtension.getTree(editor.document);
+function provideDocumentFormattingEdits(
+    parseTreeExtension: ParseTreeExtension,
+    document: TextDocument,
+    options: FormattingOptions,
+    formatter: LanguageFormatter
+): TextEdit[] {
+    const tree = parseTreeExtension.getTree(document);
 
     if (tree.rootNode.hasError()) {
         return [];
     }
 
-    const formatter = new TalonFormatter(editor);
-    const newText = formatter.getText(tree.rootNode);
-    const originalText = editor.document.getText();
+    const indentation = options.insertSpaces ? new Array(options.tabSize).fill(" ").join("") : "\t";
+    const eol = document.eol === EndOfLine.LF ? "\n" : "\r\n";
+    const newText = formatter.getText(indentation, eol, tree.rootNode);
 
-    if (originalText === newText) {
+    if (document.getText() === newText) {
         return [];
     }
 
     return [
         TextEdit.replace(
             new Range(
-                editor.document.lineAt(0).range.start,
-                editor.document.lineAt(editor.document.lineCount - 1).range.end
+                document.lineAt(0).range.start,
+                document.lineAt(document.lineCount - 1).range.end
             ),
             newText
         )
     ];
 }
 
-function getIndentation(editor: TextEditor): string {
-    return new Array(editor.options.tabSize ?? 4)
-        .fill(editor.options.insertSpaces ? " " : "\t")
-        .join("");
-}
-
-function getEOL(editor: TextEditor): string {
-    return editor.document.eol === EndOfLine.LF ? "\n" : "\r\n";
-}
-
 export function registerLanguageFormatter(parseTreeExtension: ParseTreeExtension): Disposable {
-    return languages.registerDocumentFormattingEditProvider(
-        { language: "talon" },
-        {
-            provideDocumentFormattingEdits: async () => {
-                try {
-                    return provideDocumentFormattingEditsTalon(parseTreeExtension);
-                } catch (ex) {
-                    const err = ex as Error;
-                    await window.showErrorMessage(err.message);
-                    console.error(err.stack);
-                }
-            }
-        }
-    );
+    return languages.registerDocumentFormattingEditProvider("talon", {
+        provideDocumentFormattingEdits: (document: TextDocument, options: FormattingOptions) =>
+            provideDocumentFormattingEdits(
+                parseTreeExtension,
+                document,
+                options,
+                new TalonFormatter()
+            )
+    });
 }
