135f8187783594b4f916e1627640cc17641d3e7f
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-07 17:48:45 +0100

    more work on undo

--- src/commands/undoPhrase.ts
+++ src/commands/undoPhrase.ts
@@ -1,57 +1,134 @@
 import * as vscode from "vscode";
 import type { CommandServerExtension } from "../typings/commandServer";
 
+interface DocumentState {
+    document: vscode.TextDocument;
+    text: string;
+}
+
+interface PhraseState {
+    version: string;
+    documents: DocumentState[];
+}
+
+const maxLimit = 10;
+
 export class UndoPhrase {
     private disposable: vscode.Disposable;
-    private phraseVersion: string | null = null;
-    private changeEvents: vscode.TextDocumentChangeEvent[] = [];
+    private phraseStates: PhraseState[] = [];
+    private lastState?: PhraseState;
+    private timeout?: NodeJS.Timeout;
 
     constructor(private commandServerExtension: CommandServerExtension) {
-        this.disposable = vscode.workspace.onDidChangeTextDocument((event) =>
-            this.onDidChangeTextDocument(event)
+        this.disposable = vscode.Disposable.from(
+            vscode.workspace.onDidChangeTextDocument((event) =>
+                this.onDidChangeTextDocument(event)
+            ),
+            vscode.workspace.onDidOpenTextDocument((document) => this.storeState(document))
         );
     }
 
     async undo() {
-        console.log(this.changeEvents);
-        // await vscode.commands.executeCommand("undo");
+        const phraseVersion = await this.commandServerExtension.signals.prePhrase.getVersion();
 
-        await Promise.all(this.changeEvents.map((event) => {}));
+        console.log("undo", phraseVersion);
+
+        if (phraseVersion == null) {
+            throw Error("Phrase level undo requires phrase version signal from the command server");
+        }
+
+        console.log(this.phraseStates);
+
+        // The last state is the current state so we need to go back one more
+        this.phraseStates.pop();
+        const phraseState = this.phraseStates.at(-1);
+
+        if (phraseState == null) {
+            return;
+        }
+
+        for (const state of phraseState.documents) {
+            await restoreDocumentState(state);
+        }
     }
-
-    // const wereEditsApplied = await editor.edit((editBuilder) => {
-    //     getSortedSelections(editor).forEach((selection, i) => {
-    //         const text = (start + i).toString();
-    //         editBuilder.replace(selection, text);
-    //     });
-    // });
-
-    // if (!wereEditsApplied) {
-    //     throw Error("Couldn't apply edits for generate range");
-    // }
 
     dispose() {
         this.disposable.dispose();
     }
 
-    private async onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent) {
+    private onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent) {
+        clearTimeout(this.timeout);
+        this.timeout = setTimeout(() => void this.storeState(event.document), 32);
+    }
+
+    private async storeState(document: vscode.TextDocument) {
         const phraseVersion = await this.commandServerExtension.signals.prePhrase.getVersion();
 
-        if (event.reason === vscode.TextDocumentChangeReason.Undo) {
-            console.log("undo");
-        } else {
-            console.log("change");
+        console.log("storeState", phraseVersion);
+
+        // This feature requires phrase version signal from the command server
+        if (phraseVersion == null) {
+            return;
         }
-        event.contentChanges.forEach((change) => {
-            console.log(`'${change.text}'`);
-            console.log(change.rangeOffset, change.rangeLength);
+
+        if (this.lastState?.version !== phraseVersion) {
+            this.lastState = {
+                version: phraseVersion,
+                documents: []
+            };
+
+            this.phraseStates.push(this.lastState);
+
+            // Remove oldest state if we've reached the limit
+            if (this.phraseStates.length > maxLimit) {
+                this.phraseStates.shift();
+            }
+        }
+
+        // Remove previous document state if it exists
+        this.lastState.documents = this.lastState.documents.filter(
+            (documentState) => documentState.document !== document
+        );
+
+        // Add current document state
+        this.lastState.documents.push({
+            document,
+            text: document.getText()
         });
-
-        if (phraseVersion !== this.phraseVersion) {
-            this.phraseVersion = phraseVersion;
-            this.changeEvents = [];
-        }
-
-        this.changeEvents.push(event);
     }
+}
+
+async function restoreDocumentState(documentState: DocumentState): Promise<void> {
+    const { document, text } = documentState;
+
+    console.log(text);
+
+    if (document.isClosed || document.getText() === text) {
+        return;
+    }
+
+    const editor = getEditorForDocument(document);
+
+    const documentRange = new vscode.Range(
+        document.lineAt(0).range.start,
+        document.lineAt(document.lineCount - 1).range.end
+    );
+
+    const wereEditsApplied = await editor.edit((editBuilder) => {
+        editBuilder.replace(documentRange, text);
+    });
+
+    if (!wereEditsApplied) {
+        throw Error("Couldn't apply edits for phrase undo");
+    }
+}
+
+function getEditorForDocument(document: vscode.TextDocument): vscode.TextEditor {
+    const editor = vscode.window.visibleTextEditors.find((editor) => editor.document === document);
+
+    if (!editor) {
+        throw Error(`Couldn't find editor for document: ${document.uri.toString()}`);
+    }
+
+    return editor;
 }
