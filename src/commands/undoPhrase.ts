2a5d3da89e5f6b42b02293a97b020c92ca665ea5
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2023-12-07 13:32:37 +0100

    refactor

--- src/commands/undoPhrase.ts
+++ src/commands/undoPhrase.ts
@@ -4,7 +4,7 @@ import type { CommandServerExtension } from "../typings/commandServer";
 export class UndoPhrase {
     private disposable: vscode.Disposable;
     private phraseVersion: string | null = null;
-    private contentChanges: vscode.TextDocumentContentChangeEvent[] = [];
+    private changeEvents: vscode.TextDocumentChangeEvent[] = [];
 
     constructor(private commandServerExtension: CommandServerExtension) {
         this.disposable = vscode.workspace.onDidChangeTextDocument((event) =>
@@ -12,9 +12,23 @@ export class UndoPhrase {
         );
     }
 
-    undo() {
-        console.log(this.contentChanges);
+    async undo() {
+        console.log(this.changeEvents);
+        // await vscode.commands.executeCommand("undo");
+
+        await Promise.all(this.changeEvents.map((event) => {}));
     }
+
+    // const wereEditsApplied = await editor.edit((editBuilder) => {
+    //     getSortedSelections(editor).forEach((selection, i) => {
+    //         const text = (start + i).toString();
+    //         editBuilder.replace(selection, text);
+    //     });
+    // });
+
+    // if (!wereEditsApplied) {
+    //     throw Error("Couldn't apply edits for generate range");
+    // }
 
     dispose() {
         this.disposable.dispose();
@@ -23,11 +37,21 @@ export class UndoPhrase {
     private async onDidChangeTextDocument(event: vscode.TextDocumentChangeEvent) {
         const phraseVersion = await this.commandServerExtension.signals.prePhrase.getVersion();
 
+        if (event.reason === vscode.TextDocumentChangeReason.Undo) {
+            console.log("undo");
+        } else {
+            console.log("change");
+        }
+        event.contentChanges.forEach((change) => {
+            console.log(`'${change.text}'`);
+            console.log(change.rangeOffset, change.rangeLength);
+        });
+
         if (phraseVersion !== this.phraseVersion) {
             this.phraseVersion = phraseVersion;
-            this.contentChanges = [];
+            this.changeEvents = [];
         }
 
-        this.contentChanges.push(...event.contentChanges);
+        this.changeEvents.push(event);
     }
 }
