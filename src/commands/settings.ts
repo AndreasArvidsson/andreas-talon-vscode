31a46318bbd8442dd7b6028b06befb3d979594e1
Andrew Dant <andrewdant17@gmail.com>
2024-05-20 11:00:37 -0400

    add configurationTarget when updating settings

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,9 +1,9 @@
-import { workspace } from "vscode";
+import { workspace, ConfigurationTarget } from "vscode";
 
 export function getSetting<T>(section: string, defaultValue?: T): T | undefined {
     return workspace.getConfiguration().get(section, defaultValue);
 }
 
-export function setSetting(section: string, value: any) {
-    return workspace.getConfiguration().update(section, value);
+export function setSetting(section: string, value: any, configurationTarget?: boolean | ConfigurationTarget) {
+    return workspace.getConfiguration().update(section, value, configurationTarget);
 }
