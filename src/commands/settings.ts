353f3de22502449bd7ad6242b7ff48b5af281df6
Andrew Dant <andrewdant17@gmail.com>
2024-05-21 16:00:36 -0400

    update settings configuration target type

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -1,9 +1,33 @@
-import { workspace, ConfigurationTarget } from "vscode";
+import * as vscode from "vscode";
 
 export function getSetting<T>(section: string, defaultValue?: T): T | undefined {
-    return workspace.getConfiguration().get(section, defaultValue);
+    return vscode.workspace.getConfiguration().get(section, defaultValue);
 }
 
-export function setSetting(section: string, value: any, configurationTarget?: boolean | ConfigurationTarget) {
-    return workspace.getConfiguration().update(section, value, configurationTarget);
+type ConfigurationTarget = "global" | "workspace" | "workspaceFolder";
+
+export function setSetting(section: string, value: any, configurationTarget?: ConfigurationTarget) {
+    return vscode.workspace
+        .getConfiguration()
+        .update(
+            section,
+            value,
+            configurationTarget != null ? getConfigurationTarget(configurationTarget) : undefined
+        );
+}
+
+function getConfigurationTarget(
+    configurationTarget: ConfigurationTarget
+): vscode.ConfigurationTarget {
+    switch (configurationTarget) {
+        case "global":
+            return vscode.ConfigurationTarget.Global;
+        case "workspace":
+            return vscode.ConfigurationTarget.Workspace;
+        case "workspaceFolder":
+            return vscode.ConfigurationTarget.WorkspaceFolder;
+        default:
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            throw new Error(`Unknown configuration target: ${configurationTarget}`);
+    }
 }
