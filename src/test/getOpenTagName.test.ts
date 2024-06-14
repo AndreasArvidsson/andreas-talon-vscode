153eb5d7c1cbc018c3acb516dbf4be63eeec9eb7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-14 18:13:42 +0200

    Made get open tag test work again on xml

--- src/test/getOpenTagName.test.ts
+++ src/test/getOpenTagName.test.ts
@@ -18,22 +18,9 @@ suite(command, () => {
         });
     }
 
+    testLanguage("xml");
     testLanguage("html");
     testLanguage("typescriptreact");
     testLanguage("javascriptreact");
     testLanguage("javascript");
-
-    // Xml errors when there is no closing tag
-    runTest({
-        title: `${command} - xml`,
-        command,
-        pre: {
-            language: "xml",
-            content: "<div><span></span></div>\n",
-            selections: [0, 11]
-        },
-        post: {
-            returnValue: "span"
-        }
-    });
 });
--- src/treeSitter/queries/xml.scm
+++ src/treeSitter/queries/xml.scm
@@ -3,3 +3,10 @@
         (Name) @startTag.name
     )
 ) @_.domain
+
+;; Xml errors when there is no closing tag
+(ERROR
+    (STag
+        (Name) @startTag.name
+    )
+) @_.domain
