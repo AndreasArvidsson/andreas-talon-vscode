13ac0cd6223c644bebdfbc17cb1a02a5e2c275d2
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2024-06-15 09:59:31 +0200

    tests

--- src/scripts/runTestsCI.ts
+++ src/scripts/runTestsCI.ts
@@ -1,7 +1,50 @@
 // import * as cp from "child_process";
-import { downloadAndUnzipVSCode, runTests } from "@vscode/test-electron";
+import {
+    downloadAndUnzipVSCode,
+    resolveCliArgsFromVSCodeExecutablePath,
+    runTests
+} from "@vscode/test-electron";
+import * as cp from "child_process";
 import * as path from "path";
 import * as fs from "fs";
+
+const extensionDependencies = [
+    // Cursorless access to Tree sitter
+    "pokey.parse-tree"
+
+    // Register necessary language-IDs for tests
+    // "scala-lang.scala", // scala
+    // "mrob95.vscode-talonscript", // talon
+    // "jrieken.vscode-tree-sitter-query", // scm
+
+    // Necessary for the `drink cell` and `pour cell` tests
+    // "ms-toolsai.jupyter",
+];
+
+function installExtensionDependencies(vscodeExecutablePath: string) {
+    const [cli, ...args] = resolveCliArgsFromVSCodeExecutablePath(vscodeExecutablePath);
+
+    // Install extension dependencies
+    const extensionInstallArgs = [
+        ...args,
+        ...extensionDependencies.flatMap((dependency) => ["--install-extension", dependency])
+    ];
+
+    console.log("starting to install dependency extensions");
+    console.log(`cli: ${cli}`);
+    console.log(JSON.stringify(extensionInstallArgs, null, 2));
+
+    const { status, signal, error } = cp.spawnSync(cli, extensionInstallArgs, {
+        encoding: "utf-8",
+        stdio: "inherit"
+    });
+
+    console.log("status: ", status);
+    console.log("signal: ", signal);
+    console.log("error: ", error);
+
+    console.log("finished installing dependency extensions");
+}
 
 export async function launchVscodeAndRunTests() {
     try {
@@ -9,39 +52,9 @@ export async function launchVscodeAndRunTests() {
         const extensionTestsPath = path.join(outDirPath, "test/testUtil/runAllTests.js");
         const extensionDevelopmentPath = outDirPath;
 
-        console.log(`extensionTestsPath: ${extensionTestsPath}`);
-        console.log(`extensionDevelopmentPath: ${extensionDevelopmentPath}`);
-
-        console.log(__dirname);
-        console.log(fs.readdirSync(__dirname));
-
-        console.log(fs.existsSync(outDirPath));
-        console.log(fs.readdirSync(outDirPath));
-
         const vscodeExecutablePath = await downloadAndUnzipVSCode("stable");
 
-        console.log(`vscodeExecutablePath: ${vscodeExecutablePath}`);
-
-        // Install extension dependencies
-        // const extensionInstallArgs = [
-        //     ...args,
-        //     ...extensionDependencies.flatMap((dependency) => ["--install-extension", dependency])
-        // ];
-
-        // console.log("starting to install dependency extensions");
-        // console.log(`cli: ${cli}`);
-        // console.log(JSON.stringify(extensionInstallArgs, null, 2));
-
-        // const { status, signal, error } = cp.spawnSync(cli, extensionInstallArgs, {
-        //     encoding: "utf-8",
-        //     stdio: "inherit"
-        // });
-
-        // console.log("status: ", status);
-        // console.log("signal: ", signal);
-        // console.log("error: ", error);
-
-        // console.log("finished installing dependency extensions");
+        installExtensionDependencies(vscodeExecutablePath);
 
         // Run the integration test
         const code = await runTests({
