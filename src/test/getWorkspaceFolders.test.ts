71e70a520e9f22e7d958085e825cd095548190b7
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2025-04-10 08:27:39 +0200

    Cleanup

--- src/test/getWorkspaceFolders.test.ts
+++ src/test/getWorkspaceFolders.test.ts
@@ -1,14 +1,15 @@
 import * as assert from "assert";
 import * as vscode from "vscode";
-import { CommandId } from "../commands/commands";
 import { getFullCommand } from "../util/getFullCommand";
 
-suite("Get Workspace Folders Command", () => {
-    test("getWorkspaceFolders returns an array of file system paths for workspace folders", async () => {
-        const command = getFullCommand("getWorkspaceFolders" as CommandId);
+const command = "getWorkspaceFolders";
+
+suite(command, () => {
+    test("Return value", async () => {
+        const fullCommand = getFullCommand(command);
 
         // Execute the command
-        const result = await vscode.commands.executeCommand(command);
+        const result = await vscode.commands.executeCommand(fullCommand);
 
         // Check that the result is an array of file system paths
         const expectedPaths = vscode.workspace.workspaceFolders?.map((folder) => folder.uri.fsPath);
